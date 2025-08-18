1ac355978871baf8baeaf3d07187f229d692298a
Your Name <you@example.com>
2025-08-18 22:19:38 +0530

    First commit

--- pattern.py
+++ pattern.py
@@ -1,32 +1,5 @@
->>> a = 10
->>> b = 20
->>> a,b
-(10, 20)
->>> b, a = a, b
->>> a
-20
->>> b
-10
->>> temp = a
->>> a = b
->>> b = temp
->>> a
-10
->>> b
-20
-
-
-
-
->>> for i in range(1, 6):  # Outer loop for rows
-...     for j in range(1, i + 1):  # Inner loop for columns
-...         print(j, end=" ")
-...     print()  # Move to the next line
-... 
-...     
-1 
-1 2 
-1 2 3 
-1 2 3 4 
-1 2 3 4 5 
-
+rows = 5 
+for i in range(1, rows +1):
+    for j in range(1, i + 1):
+        print(j, end= ' ')
+    print('')
